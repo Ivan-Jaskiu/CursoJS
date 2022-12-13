e1e53f4f67506cef41af9e573c5796bddbece29c
Ivan <ivanjaskiu@gmail.com>
2022-12-13 14:10:30 -0300

    llops

---
+++ Logica/Aula23/curtoCircuito.js
@@ -0,0 +1,24 @@
+/*
+
+Falso
+> 0
+> ''  ""  ``
+> NaN
+> null/undefined
+
+*/
+
+// CurtoCIrcuito
+function falaOi () {
+    return 'OI';
+}
+
+const vaiExecutar = 'jao';
+console.log(falaOi() && vaiExecutar);
+
+
+// OR
+console.log(0 || false || null || 'Luiz Otavio' || true)
+
+
+
