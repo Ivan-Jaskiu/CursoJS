6e8bcfeae199f0de76eefc6a239bbd143fa7f4a4
Ivan <ivanjaskiu@gmail.com>
2022-12-19 10:54:51 -0300

    For

---
+++ Logica/Aula34/forIn.js
@@ -0,0 +1,30 @@
+const frutas = ['pera', 'franbuesa'];
+
+
+    //pegado os valores atravez do for IN
+// for (let indice in frutas){
+//     console.log(frutas[indice]);
+// }
+
+
+const pessoa = {
+    nome: 'Ivan',
+    sobrenome: 'Jaskiu',
+    idade: 25,
+};
+
+for (let chave in pessoa){
+    console.log(chave, pessoa[chave]);
+}
+
+// console.log(pessoa.nome);
+// console.log(pessoa['nome']);
+
+
+
+
+
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
